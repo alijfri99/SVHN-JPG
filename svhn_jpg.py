2ec240d18fcc5d39c0266ffb37caa6aac6971db0
Ali Jafari <alijfri99@gmail.com>
2023-03-28 10:53:30 +0330

    Corrected the extraction process to normalize the values of images to [0, 255].

--- svhn_jpg.py
+++ svhn_jpg.py
@@ -10,6 +10,9 @@ def extract_dataset(filename):
     x = numpy.moveaxis(x, -1, 0)
     
     for index in range(len(y)):
+        temp_image = (x[index] - numpy.min(x[index])).astype(float) * 255 / (numpy.max(x[index]) - numpy.min(x[index])).astype(float)
+        temp_image = numpy.round(temp_image)
+        temp_image = temp_image.astype(numpy.uint8)
         image = Image.fromarray(x[index])
         image.save(os.path.join('Dataset', filename[:-4], str((y[index][0]) % 10), str(index) + '.jpg'))
 
@@ -17,4 +20,4 @@ def extract_dataset(filename):
 
 extract_dataset('training.mat')
 extract_dataset('testing.mat')
-extract_dataset('extra.mat')
+# extract_dataset('extra.mat')
